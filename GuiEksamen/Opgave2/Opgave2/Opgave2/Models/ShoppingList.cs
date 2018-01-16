9bd0be6a32236858b84a5e334587e2facc26515b
Tobias Kjær Henriksen <tobi.dk@gmail.com>
2018-01-16 20:46:03 +0100

    Started Opgave2

---
+++ GuiEksamen/Opgave2/Opgave2/Opgave2/Models/ShoppingList.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Opgave2.Models
+{
+    public class ShoppingList
+    {
+        public List<Product> ProductList { get; set; }
+        public string Name { get; set; }
+        public string Id { get; set; }
+    }
+}
