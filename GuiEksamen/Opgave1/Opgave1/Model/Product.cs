a0cd8cf2303ed80d2215567dbd0820a960dda598
Tobias Kjær Henriksen <tobi.dk@gmail.com>
2018-01-16 09:22:27 +0100

    Started Assignment

---
+++ GuiEksamen/Opgave1/Opgave1/Model/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opgave1.Model
+{
+    class Product: AProduct
+    {
+        public string ProductCode { get; set; }
+        public string Name { get; set; }
+        public string Price { get; set; }
+    }
+}
